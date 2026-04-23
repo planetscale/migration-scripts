a11b458088e3587680de02d9e845f41291e045a9
Chris Munns <munns@planetscale.com>
2026-04-23 17:24:26 -0400

    Add EC2 Instance Connect CIDR rules to AWS templates

--- pgcopydb-templates/aws-terraform/pgcopydb-migration-instance.tf
+++ pgcopydb-templates/aws-terraform/pgcopydb-migration-instance.tf
@@ -109,6 +109,43 @@ locals {
     12000 = 70000
   }
 
+  # Source: https://ip-ranges.amazonaws.com/ip-ranges.json (service: EC2_INSTANCE_CONNECT)
+  ec2_instance_connect_cidrs = {
+    "af-south-1"     = "13.244.121.196/30"
+    "ap-east-1"      = "43.198.192.104/29"
+    "ap-northeast-1" = "3.112.23.0/29"
+    "ap-northeast-2" = "13.209.1.56/29"
+    "ap-northeast-3" = "15.168.105.160/29"
+    "ap-south-1"     = "13.233.177.0/29"
+    "ap-south-2"     = "18.60.252.248/29"
+    "ap-southeast-1" = "3.0.5.32/29"
+    "ap-southeast-2" = "13.239.158.0/29"
+    "ap-southeast-3" = "43.218.193.64/29"
+    "ap-southeast-4" = "16.50.248.80/29"
+    "ap-southeast-5" = "43.216.87.48/29"
+    "ap-southeast-7" = "43.209.155.96/29"
+    "ca-central-1"   = "35.183.92.176/29"
+    "ca-west-1"      = "40.176.213.168/29"
+    "eu-central-1"   = "3.120.181.40/29"
+    "eu-central-2"   = "16.63.77.8/29"
+    "eu-north-1"     = "13.48.4.200/30"
+    "eu-south-1"     = "15.161.135.164/30"
+    "eu-south-2"     = "18.101.90.48/29"
+    "eu-west-1"      = "18.202.216.48/29"
+    "eu-west-2"      = "3.8.37.24/29"
+    "eu-west-3"      = "35.180.112.80/29"
+    "il-central-1"   = "51.16.183.224/29"
+    "me-central-1"   = "3.29.147.40/29"
+    "me-south-1"     = "16.24.46.56/29"
+    "mx-central-1"   = "78.12.207.8/29"
+    "sa-east-1"      = "18.228.70.32/29"
+    "us-east-1"      = "18.206.107.24/29"
+    "us-east-2"      = "3.16.146.0/29"
+    "us-west-1"      = "13.52.6.112/29"
+    "us-west-2"      = "18.237.140.160/29"
+  }
+  ec2_instance_connect_cidr = lookup(local.ec2_instance_connect_cidrs, var.region, "18.206.107.24/29")
+
   tags = {
     Name    = "${var.resource_prefix}-ec2"
     Purpose = "PostgreSQL to PlanetScale Migration"
@@ -186,11 +223,20 @@ resource "aws_vpc_security_group_egress_rule" "all_outbound" {
 
 resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.pgcopydb.id
-  description       = var.your_public_ip != "" ? "SSH access restricted to ${var.your_public_ip}" : "SSH access open to all (specify your_public_ip to restrict)"
+  description       = var.your_public_ip != "" ? "SSH restricted to ${var.your_public_ip}" : "SSH open to all (specify your_public_ip to restrict)"
   ip_protocol       = "tcp"
   from_port         = 22
   to_port           = 22
   cidr_ipv4         = var.your_public_ip != "" ? "${var.your_public_ip}/32" : "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_instance_connect" {
+  security_group_id = aws_security_group.pgcopydb.id
+  description       = "EC2 Instance Connect service IP range for ${var.region}"
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_ipv4         = local.ec2_instance_connect_cidr
 }
 
 resource "aws_vpc_security_group_ingress_rule" "icmp_dest_unreachable" {
